0047bd36eea62ed4b5477f78bc39b8cd5028340c
Gabriel Rodríguez <rgabriel@mit.edu>
2024-10-09 23:44:04 -0400

    Update blurb

--- src/lib/AboutDormspam.svelte
+++ src/lib/AboutDormspam.svelte
@@ -6,23 +6,38 @@
 <p>Do you want to stay informed about what happens on campus?</p>
 
 <details>
-	<summary> What is this? (click for more info) </summary>
+	<summary> Click for frequently asked questions </summary>
 
-	<!-- copied from how to dormspam -->
-	<p>Dormspam is the act of emailing the social lists of MIT dorms and the IFC, and is used for advertising events, club announcements, surveys, and more.</p>
-
-	<strong>Pros</strong>
+	<strong>What is dormspam?</strong>
 	<ul>
-		<li>Some dorms' dormspam lists also double as the intra-dorm communication, so you also get to hear
-			about what is happening in your own dorm.</li>
-		<li>Get to hear about events going on.</li>
-		<li>Dormspam is great. TODO get the text to put here from Jordan and Ananda.</li>
+		<li>Dormspam is a bunch of email lists which you can send to (and receive from!), in order to get
+			information about clubs, events on campus, sales, and more!</li>
+		<li>The email lists are organized by dorm/living group. This website exists to centralize joining
+			dormspam into one place.</li>
+		<li>Dormspam, and bulk email lists in general, have a
+			<a href="https://mitadmissions.org/blogs/entry/mailing-list-culture/">long</a>
+			<a href="https://mitadmissions.org/blogs/entry/an_explanation/">history</a>
+			at MIT, and is a really important part of MIT culture! You can join for now if you want to
+			figure out what dormspam is all about, and so you don't miss out on anything, and if you decide
+			later that you want to filter it or stop receiving it altogether, that's okay too! You can come
+			back to this website if you want to unsubscribe!</li>
 	</ul>
 
-	<strong>Cons</strong>
+	<strong>What kinds of emails get sent to dormspam?</strong>
 	<ul>
-		<li>It is very spammy, but you can <a href="https://sites.mit.edu/how-to-dormspam/filter/" target="_blank">easily filter</a> it though.</li>
-		<li>Sometimes there are flame wars.</li>
+		<li>Anything! Most emails are advertisements such as performances/events or club meetings (often with free food or boba!), but other common emails include sales (with cheap furniture/clothes/AC units), job/UROP opportunities, and silly surveys.		</li>
+		<li>The great thing about dormspam is that anyone can send anything they want, and the lists are generally unmoderated. That means that sometimes, controversial topics that you disagree with may get discussed. But of course, you can filter your email however you want to, and you don't need to read emails that you don't want to engage with!</li>
+	</ul>
+
+	<strong>How do I send emails to dormspam?</strong>
+	<ul>
+		<li><a href="https://how-to-dormspam.mit.edu">dormspam.mit.edu</a> is a great resource which tells you all the norms for sending dormspam emails correctly, so people can filter properly!</li>
+	</ul>
+
+	<strong>What dormspam list should I be on?</strong>
+	<ul>
+		<li>This website automatically identifies which dorm you live in, and tells you how to join that list.</li>
+		<li>If you don't live in a dorm, it will redirect you to <code>random-hall-dormspam</code>, which is run by Random Hall, but is open to all current undergraduate and graduate students. <code>random-hall-dormspam</code> does not get any traffic other than dormspam.</li>
 	</ul>
 </details>
 
