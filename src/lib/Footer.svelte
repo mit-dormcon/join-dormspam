f479a99d5175142ccfa48be15290db22947a5e8b
Gabriel Rodríguez <rgabriel@mit.edu>
2024-11-09 17:32:00 -0500

    Add logos

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,9 @@
+<script>
+
+</script>
 <footer>
-	<a href="https://accessibility.mit.edu"> Accessibility </a>
+	<p><a href="https://accessibility.mit.edu"> Accessibility </a></p>
+	<a target="_blank" href="https://dormcon.mit.edu"><img src="https://dormcon.mit.edu/img/dormcon-sticker.svg" height="100" width="100" alt="DormCon shark. Click to open DormCon website"></a>
+	<a target="_blank" href="https://sipb.mit.edu"><img src="https://sipb.mit.edu/images/logo.png" alt="SIPB fuzzball. Click to open SIPB website" height="100" width="70"></a>
+	<p>Contact: <a href="mailto:join-dormspam@mit.edu">join-dormspam at mit.edu</a></p>
 </footer>
