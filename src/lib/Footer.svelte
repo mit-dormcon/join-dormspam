b113e3829776027e95d535a8cc2af89615d74e8f
Gabriel Rodríguez <rgabriel@mit.edu>
2024-11-09 20:56:35 -0500

    add background to logos on dark theme

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,9 +1,16 @@
-<script>
-
-</script>
 <footer>
 	<p><a href="https://accessibility.mit.edu"> Accessibility </a></p>
-	<a target="_blank" href="https://dormcon.mit.edu"><img src="https://dormcon.mit.edu/img/dormcon-sticker.svg" height="100" width="100" alt="DormCon shark. Click to open DormCon website"></a>
-	<a target="_blank" href="https://sipb.mit.edu"><img src="https://sipb.mit.edu/images/logo.png" alt="SIPB fuzzball. Click to open SIPB website" height="100" width="70"></a>
+	<p id="logos">
+		<a target="_blank" href="https://dormcon.mit.edu"><img src="https://dormcon.mit.edu/img/dormcon-sticker.svg" height="100" width="100" alt="DormCon shark. Click to open DormCon website"></a>
+		<a target="_blank" href="https://sipb.mit.edu"><img src="https://sipb.mit.edu/images/logo.png" alt="SIPB fuzzball. Click to open SIPB website" height="100" width="72"></a>
+	</p>
 	<p>Contact: <a href="mailto:join-dormspam@mit.edu">join-dormspam at mit.edu</a></p>
 </footer>
+<style>
+	/* So you can actually read the text "Dormitory Council" and see the fuzzball */
+	@media (prefers-color-scheme: dark) {
+		img {
+			background-color: white;
+		}
+	}
+</style>
