b91c13931522d4345da675bbd6ddffb0c2bdcc7f
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-01-09 19:12:37 -0500

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess({ script: true }),
-
 	kit: {
-		// https://kit.svelte.dev/docs/single-page-apps#prerendering-individual-pages
 		adapter: adapter({
 			fallback: 'index.html'
 		})
-		//paths: {base: '/join-dormspam'},
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	}
 };
 
