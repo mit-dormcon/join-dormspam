2e67a847144811d8c196fb4303acb5a18c5787cf
Gabriel R. <rgabriel@mit.edu>
2026-06-21 18:11:04 -0400

    Add notice about site being broken

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<style>
+	#issue-warning {
+		background-color: rgb(255, 204, 204);
+	}
+</style>
+
 <script lang="ts">
 	import { setWebathena, setTicket, setEmailToken, setUsername } from '$lib/context';
 	import {
@@ -40,6 +46,15 @@
 
 <main>
 	<AboutDormspam />
+
+	<!-- TODO: remove this notice once it is fixed -->
+	<div class="notice" id="issue-warning">
+		<p>We are aware of issues with this site and we are working to fix them.</p>
+		<p>
+			For the time being, if you want to join dormspam,
+			<strong>please follow the instructions at <a href="https://sites.mit.edu/how-to-dormspam/unsubscribe/">how-to-dormspam.mit.edu</a></strong>.
+		</p>
+	</div>
 
 	{#if $webathena === null}
 		<p>
